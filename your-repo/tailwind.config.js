66266b0b8ae7e3adc42a7bd718f201a29a9a3ffb
HuyIT2706 <buivanhuy2706@gmail.com>
2025-04-25 07:46:10 +0700

    Move index.html to root for GitHub Pages

--- your-repo/tailwind.config.js
+++ your-repo/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./docs/**/*.{html,js}"],
+  content: ["./your-repo/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
     extend: {
